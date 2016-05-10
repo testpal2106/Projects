bd69bc2e01437caddbed5512cfd0eeb23e554cc7
csolution <csolution@csolution-HCL-Desktop.(none)>
2016-05-10 17:17:20 +0530

    made changes in the core folder

---
+++ core/assets/js/custom.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	signupValidate();
+});
+
+var validateEmail = function (email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+var signupValidate = function(){
+	
+jQuery.validator.addMethod("validateEmail", function(value, element) {						
+	var status = validateEmail(value);
+	return status; 
+}, "Please enter valid email address.");
+	
+$("#signup_form").validate({
+		'errorClass': 'error_input',
+		'errorElement': 'span',
+		errorPlacement: function(error, element) {				
+			var common_error = '<div class="alert alert-danger text-font"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><i class="fa fa-close icon-spce"></i><span> Fields marked in red are mandatory.</span></div>';
+			error.insertAfter($(element)); 
+		},
+		rules: {
+			user_login: {required:true,rangelength:[2,6],
+			  remote: {
+				url: "ajaxHandlers/check-username.php",
+				type: "post",
+				data: {
+				  user_login: function() {
+					return $( "#user_login" ).val();
+				  }
+				}
+			  }
+
+			},				
+			first_name: "required",				
+			last_name: "required",				
+			password: {required:true,rangelength:[2,6] },				
+			confirm_password: {required:true, equalTo:'#password'},				
+			user_email: {required:true, validateEmail:true},				
+			phone_no: {required:true, digits:true},				
+		},
+		messages: {
+			user_login: {required:'',rangelength:'Please enter username in the range of 2-6 characters.',
+			remote: 'This username already exists. Please try another.'
+			},	
+			first_name: "",
+			last_name: "",
+			password: {required:'',rangelength:'Please enter password in the range of 2-6 characters.'},
+			confirm_password: {required:'', equalTo:'Both passwords do not match.'},
+			user_email: {required:'', digits:'Please enter a valid email.'},	
+			phone_no: {required:'', digits:'Please enter digits only.'},	
+			
+			
+		},
+		submitHandler: function(form){
+			form.submit();
+		}
+	});
+
+
+}
+/*
+$(document).on('click', '.login-btn', function(){
+	signupValidate();
+});
+*/
--- core/includes/classes/application.class.php
+++ core/includes/classes/application.class.php
@@ -1,5 +1,285 @@
 <?php
-Class application extends db{
-	public $
+
+/**
+ * Application - The main class
+ *
+ */
+Class Application extends db
+{
+
+/*
+ * Class Constants & Properties
+*/
+    const APPLICATION_SALT = 'core';
+    public $access_table = '';
+    public $ses_name = 'frontend_admin_logged_in';
+    public $logged_session_id = 'frontend_logged_id';
+    public $logged_session_username = 'frontend_username';
+    public $logged_session_email = 'frontend_user_email';
+ 	public $logged_session_name ='frontend_name';
+        
+    private static $appInstance;
+
+/*
+ * Public Methods
+ */
+    /*
+     * Constructor
+     */
+     public function __construct() {
+      parent::__construct('localhost', 'root', 'root');
+        $this->selectdb('core');
+    } 
+
+
+    /*
+     * Singleton Instance 
+     */
+	public function getInstance() {
+	
+		if(!self::$appInstance) {
+			
+			self::$appInstance = new Application();
+		}
+		
+		return self::$appInstance;
+	}
+	
+    /*
+     * method to validate login
+     */
+     public function login() { 
+
+        $_SESSION[$this->ses_name] = false;
+        $_SESSION[$this->logged_session_id] = '';
+        $_SESSION['msg'] = '';
+        $return_array = array();
+
+        if(func_num_args() > 1) {
+            $args = func_get_args();
+            $return_array = $this->authenticate($args[0], $args[1]);
+
+            if(is_array($return_array)) {
+
+                $_SESSION[$this->ses_name] = true;
+                $_SESSION[$this->logged_session_id] = $return_array[0]['id'];
+				$_SESSION[$this->logged_session_name] = $return_array[0]['first_name'] . ' ' . $return_array[0]['last_name'];
+                $_SESSION[$this->logged_session_username] = $return_array[0]['username'];
+                $_SESSION[$this->logged_session_email] = $return_array[0]['email'];
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    /*
+     * method for logout operation
+     */
+
+     public function logout() {
+
+		if(count($_SESSION) > 0) {
+			foreach($_SESSION as $key => $value) {
+				if(preg_match("/frontend_/", $key)) {
+					unset($_SESSION[$key]);
+				}	
+			}
+		}	
+		 
+    }
+
+    /*
+     * method used to check whether the user logged in or not
+     */
+
+     public function is_logged_in() {
+        if($_SESSION[$this->ses_name] === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     * method used for SELECT query operations
+     */
+
+    public function find($select = array('*'), $params = array('conditions' => array(), 'sort' => '', 'limit' => '', 'group' => '')) {
+
+        $where_condition = array();
+        $sort_order = '';
+        $limit = '';
+        $group_by = '';
+        $params['conditions'] = (count($params['conditions']) > 0) ? $params['conditions'] : array('1 = ' => '1');
+
+        foreach ($params['conditions'] as $k => $v ) $where_condition[] = "$k '".$this->escape(trim($v))."'";
+
+        if(!empty($params['sort'])) {
+            $sort_order .= 'ORDER BY ' . $params['sort'];
+        }
+
+        if(!empty($params['limit'])) {
+            $limit .= 'LIMIT ' . $params['limit'];
+        }
+
+        if(!empty($params['group'])) {
+            $group_by .= 'GROUP BY ' . $params['group'];
+        }
+ 
+
+
+         $result_array = $this->getrows('SELECT '
+                        .implode(', ', $select).'
+                        FROM
+                        '.$this->access_table.'
+                        WHERE
+                        '.implode(' AND ', $where_condition)
+                        .' '.$sort_order
+                        .' '.$limit
+                        .' '.$group_by
+                       );
+        return $result_array;
+    }
+
+    /*
+     * method used for COUNT() query operations
+     */
+
+   public function find_count($params = array('conditions' => array())) {
+
+        $where_condition = array();
+        $params['conditions'] = (count($params['conditions']) > 0) ? $params['conditions'] : array('1 = ' => '1');
+        foreach ($params['conditions'] as $k => $v ) $where_condition[] = "$k '".$this->escape(trim($v))."'";
+
+        $result_array = $this->getrows('SELECT COUNT(id) AS count FROM '
+                        .$this->access_table.'
+                        WHERE
+                        '.implode(' AND ', $where_condition)
+                       );
+        return $result_array[0]['count'];
+    } 
+	   
+
+    /*
+     * method used to INSERT records into database
+     */
+
+    public function add($data) {
+
+
+        foreach ($data as $k => $v ) {
+            if(strpos($k, 'password') !== false) {
+                $v = $this->encrypt_password(trim($v));
+            }
+            $data[$k] = "'".$this->escape(trim($v))."'";
+        }
+ 
+        $this->execute("INSERT INTO
+                        ".$this->access_table."
+                        (`".implode('`, `', array_keys($data))."`)
+                        VALUES (".implode(", ", $data).")");
+    }
+
+    /*
+     * method used to UPDATE records in the database
+     */
+
+     public function save($data, $where = array(1 => 1)) {
+	
+        foreach ($data as $k => $v ) {
+            if(strpos($k, 'password') !== false) {
+                $v = $this->encrypt_password(trim($v));
+            }
+            $update_data[] = "$k = '".$this->escape(trim($v))."'";
+        }
+
+        foreach ($where as $k => $v ) $where_condition[] = "$k '".$this->escape($v)."'";
+ 
+
+        $this->execute("UPDATE  ".$this->access_table." SET
+                       ".implode(', ', $update_data)." WHERE
+                                     ".implode(' AND ', $where_condition));
+
+    }
+
+    /*
+     * method used to DELETE records from the database
+     */
+
+    public function delete($where) {
+ 
+		foreach ($where as $k => $v )  $where_condition[] = "$k IN ('".$v."')";
+		
+		$this->execute("DELETE FROM ".$this->access_table." WHERE ".implode(' AND ', $where_condition)); 
+		
+    }
+
+    public function __destruct() {
+        parent::__destruct();
+    }
+
+
+/*
+ * Private Methods
+ */
+
+    /*
+     * method used to authenticate user login
+     */
+    private function authenticate($username, $password)
+    {
+        $user = $this->find(array('*'), $params = array('conditions' => array('username = ' => $username)));
+
+        if(count($user) > 0)
+        {
+            $expected_password = $this->encrypt_password($password);
+
+            if(strcmp($user[0]['password'], $expected_password) != 0)
+            {
+                $_SESSION['msg'] = 'Invalid Password!';
+                return false;
+            }
+            else
+            {
+                if($user[0]['status'] != '1')
+                {
+                    $_SESSION['msg'] = 'Account Blocked!';
+                    return false;
+                }
+                else
+                {
+                    return $user;
+                }
+            }
+        }
+        else
+        {
+            $_SESSION['msg'] = 'Invalid Email!';
+            return false;
+        }
+    }
+
+    /*
+     * method used to encrypt passwords
+     */
+
+     public function encrypt_password($password) { 
+ 
+        return base64_encode($password . '-' . self::APPLICATION_SALT);
+    }
+
+    /*
+     * method used to decrypt passwords
+     */
+
+    public function decrypt_password($password) {
+        $decode_password = explode("-", base64_decode($password));
+        return $decode_password[0];
+    }
+ 
+
+
 	
 }
+?>
